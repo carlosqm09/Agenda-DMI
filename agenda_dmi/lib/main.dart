92747bcb6201721e560e564e624caa7b4fc81c65
Eduardo Gamboa <manuelgambe99@gmaiil.com>
2021-10-12 23:57:56 -0500

    Update main.dart

--- agenda_dmi/lib/main.dart
+++ agenda_dmi/lib/main.dart
@@ -5,12 +5,18 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:agenda_dmi/pages/home.dart';
 
-void main(List<String> args) async {
-  runApp(MyApp());
-  final prefs = PreferenciasUsuario.instance;
+// void main(List<String> args) async {
+//   runApp(MyApp());
+//   final prefs = PreferenciasUsuario.instance;
 
-  await Firebase.initializeApp();
-  await prefs.initPrefs();
+//   await Firebase.initializeApp();
+//   await prefs.initPrefs();
+// }
+
+void main() {
+  runApp(new MaterialApp(
+    home: new HomePage(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
